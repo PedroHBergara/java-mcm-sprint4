e791b8a5a39de4a87d982d1283e1ec4bccecaa29
PedroHBergara <pedrobergara5@gmail.com>
2025-11-04 15:57:25 -0300

    first commit

---
+++ src/test/java/br/com/fiap/api/mcm_java/McmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api.mcm_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McmJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
