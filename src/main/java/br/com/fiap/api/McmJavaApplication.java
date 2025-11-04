2181e64c946a407a2c651b7506a660f42811e55a
PedroHBergara <pedrobergara5@gmail.com>
2025-11-04 16:28:59 -0300

    versao estavel

--- src/main/java/br/com/fiap/api/McmJavaApplication.java
+++ src/main/java/br/com/fiap/api/McmJavaApplication.java
@@ -1,4 +1,4 @@
-package br.com.fiap.api.mcm_java;
+package br.com.fiap.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
